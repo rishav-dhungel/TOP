0c86f9054a06c0b65796390cc18edef1e0ba60b1
rishav <rishavdhungel1@gmail.com>
2025-09-24 13:01:05 +0545

    feat: routes and route params

--- Project: Basic Informational Site/index.js
+++ Project: Basic Informational Site/index.js
@@ -25,6 +25,43 @@ server.get("/contact", (req,res) =>{
   res.sendFile(path.join(__dirname,'public','contact-me.html'));
 })
 
+//routes
+server.get("/message{s}", (req,res)=>{
+  res.send("This route works for either 'message' or 'messages' ");
+})
+
+
+server.get("/message{s}/{*splat}/chat", (req,res)=>{
+  res.send("This route works for either messages/{anything}/chat or /message/chat")
+})
+
+server.get("/user/:userId", (req,res) =>{
+  res.send(`You have reached the call for User with the id of: ${req.params.userId}`)
+  console.log(req.params)
+  res.end();
+})
+
+
+server.get("/:userId/messages", (req,res) =>{
+  console.log(req.params);
+  res.json(req.params)
+  res.end();
+})
+
+
+server.get("/:userId/messages/:messageId", (req,res) =>{
+  console.log(req.params)
+  res.json(req.params)
+  res.end;
+})
+
+
+server.get("/{*splat}", (req,res)=>{
+  res.status(404).sendFile(path.join(__dirname, 'public', '404.html'));
+
+})
+
+
 
 // 404 handler (catch-all) - should be last
 server.use((req, res) => {
